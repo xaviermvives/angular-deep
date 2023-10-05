a3a0a5c4a3d28d1782c7be7794ccdb7776b89fa8
xavier <montjuic79@gmail.com>
2023-10-05 16:46:18 +0200

    Added angular material

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  imports: [HttpClientModule, RouterModule],
+  imports: [HttpClientModule, RouterModule, MatTabsModule],
   declarations: [NotFoundComponent],
-  exports: [HttpClientModule, RouterModule, NotFoundComponent],
+  exports: [HttpClientModule, RouterModule, NotFoundComponent, MatTabsModule],
   providers: [],
 })
 export class SharedModule {
